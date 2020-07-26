f7a77d0d79135a47a9041b943f249df8d3dfda00
Your Name <snmangalam2708@gmail.com>
2020-07-26 12:02:35 -0400

    2. Objects

--- app/js/index.js
+++ app/js/index.js
@@ -7,12 +7,31 @@ import { log, logTitle } from 'logger';
 logTitle('Variables')
 /* coding examples */
 
-var name = "Maria Jones";
-var age = 21;
-var hasDriverLicense = true;
-var empty = undefined;
+// var name = "Maria Jones";
+// var age = 21;
+// var hasDriverLicense = true;
+// var empty = undefined;
 
-log(name + " " + "type = " + typeof name);
-log(age + " " + "type = " + typeof age);
-log(hasDriverLicense + " " + "type = " + typeof hasDriverLicense);
-log(empty + " " + "type = " + typeof empty);
+// log(name + " " + "type = " + typeof name);
+// log(age + " " + "type = " + typeof age);
+// log(hasDriverLicense + " " + "type = " + typeof hasDriverLicense);
+// log(empty + " " + "type = " + typeof empty);
+
+var person = {
+    name: "Maria Jones",
+    age: 21,
+    hasDriverLicense: true,
+    dateOfBirth: "01/01/2000",
+    address: {
+
+        firstLine: "230 Burham St",
+        city: "Cleveland",
+        state: "Ohio"
+    }
+};
+
+// log(person+"\n"); 
+// log(JSON.stringify(person)+"\n")
+// log(person.name+"\n")
+log(JSON.stringify(person.address)+"\n")
+log(Object.values(person)+"\n")
