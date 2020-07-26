84f592d7fd820bc231d547585da97ee0eee26c44
Your Name <snmangalam2708@gmail.com>
2020-07-26 11:51:33 -0400

    1. Variables

--- app/js/index.js
+++ app/js/index.js
@@ -4,5 +4,15 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Title')
+logTitle('Variables')
 /* coding examples */
+
+var name = "Maria Jones";
+var age = 21;
+var hasDriverLicense = true;
+var empty = undefined;
+
+log(name + " " + "type = " + typeof name);
+log(age + " " + "type = " + typeof age);
+log(hasDriverLicense + " " + "type = " + typeof hasDriverLicense);
+log(empty + " " + "type = " + typeof empty);
